aff016b6471398951768261166be4a8eafe02ffb
GT <17635035787>
2019-08-19 18:02:58 +0800

    未做具体修改

--- User/manager.py
+++ User/manager.py
@@ -16,6 +16,6 @@ def not_found(e):
     return render_template('test.html')
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('0.0.0.0', 8080), app)
-    http_server.serve_forever()
-    # app.run('127.0.0.1', 8080)
+    # http_server = WSGIServer(('0.0.0.0', 8080), app)
+    # http_server.serve_forever()
+    app.run('127.0.0.1', 8080)
