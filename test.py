a45f642338adea24af8ef85c70e754d0890e78b6
GT <17635035787>
2019-08-21 17:56:32 +0800

    未做具体修改

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
-import uuid
 
-uuid_str = uuid.uuid4()
-tmp_file_name = 'img-%s.png' % uuid_str
-print(tmp_file_name)
+# 文件名称随机
+# import uuid
+#
+# uuid_str = uuid.uuid4()
+# tmp_file_name = 'img-%s.png' % uuid_str
+# print(tmp_file_name)
+
+# 发送验证码
+
+import requests
+from urllib.parse import urlencode
+
+#
+data = {'name':'冠通期货','pwd':'GTQH0037'}
+url = 'http://www.139000.com/send/getfee.asp?'+urlencode(data)
+url1 = 'http://www.139000.com/send/gsend.asp?name=冠通期货&pwd=GTQH0037&dst=17635035787&msg=测试'
+res = requests.get(url1)
+print(res.status_code)
+print(res.headers)
+print(res.text)
